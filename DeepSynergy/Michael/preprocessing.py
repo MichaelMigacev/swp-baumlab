e301724b1f24592609c2e77cea85a7d56ef99dd4
MFlan019 <mflan019@gmail.com>
2025-08-26 16:42:47 +0200

    Update preprocessing.py

--- DeepSynergy/Michael/preprocessing.py
+++ DeepSynergy/Michael/preprocessing.py
@@ -15,6 +15,7 @@ class Preprocessor:
     
     @staticmethod    
     def normalize(X, means1=None, std1=None, means2=None, std2=None, feat_filt=None, norm='tanh_norm'):
+        #print("Original shape:", X.shape)
         if std1 is None:                                                      #If std1 not given: calculate
             std1 = np.nanstd(X, axis=0)
         if feat_filt is None:                                                 #If standard deviation is 0 (non informative) throw data away
@@ -36,10 +37,7 @@ class Preprocessor:
                 std2 = np.std(X, axis=0)
             X = (X-means2)/std2
             X[:,std2==0]=0
-
-            # print("Original shape:", X.shape)
-            # print("Features kept:", np.sum(feat_filt))
-
+            #print("Features kept:", np.sum(feat_filt))
             return(X, means1, std1, means2, std2, feat_filt)
     
     @staticmethod  
@@ -66,6 +64,21 @@ class Preprocessor:
         labels = self.load_labels(labels_path)
         X = self.load_features(features_path)
         
+        #For Sample Names
+        index_names = labels.index.tolist()
+        
+        # Feature-Origin creations according to paper
+        feature_temp = (
+            ["ECFP_6"] * 1309 +
+            ["phys-chem"] * 802 +
+            ["toxicophore"] * 2276 +
+            ["ECFP_6"] * 1309 +
+            ["phys-chem"] * 802 +
+            ["toxicophore"] * 2276 +
+            ["genomic"] * 3984
+        )
+        feature_group = np.array(feature_temp)
+        
         # Feature-Origin creations according to paper
         feature_origin = (
             ["ECFP_6"] * 1309 +
@@ -76,10 +89,9 @@ class Preprocessor:
             ["toxicophore"] * 2276 +
             ["genomic"] * 3984
         )
-        feature_origin = np.array(feature_origin)
 
-        #For Sample Names
-        index_names = labels.index.tolist()
+        # Add index to each feature name
+        feature_origin = np.array([f"{name}_{i}" for i, name in enumerate(feature_origin)])
 
         print(f"Creating folds using: \nnorm: {norm_method}\ntest_fold: {self.test_fold}\nval_fold: {self.val_fold}\n")
         # Get indices for different splits
@@ -107,27 +119,29 @@ class Preprocessor:
             # First normalize X_tr to obtain feat_filt (non-informative feature filter)
             X_tr, mean, std, mean2, std2, feat_filt = self.normalize(X_tr, norm=norm_method)
             # Filter feature origin list in sync with the removal of non-informative features
-            filtered_feature_origin = feature_origin[feat_filt]
+            f_feature_origin = feature_origin[feat_filt]
+            f_feature_group = feature_group[feat_filt]
 
             # Then normalize the other datasets using the same parameters and feat_filt
-            X_val, _, _, _, _, _ = self.normalize(X_val, mean, std, mean2, std2, feat_filt=feat_filt, norm=norm_method)
-            X_train, _, _, _, _, _ = self.normalize(X_train, mean, std, mean2, std2, feat_filt=feat_filt, norm=norm_method)
-            X_test, _, _, _, _, _ = self.normalize(X_test, mean, std, mean2, std2, feat_filt=feat_filt, norm=norm_method)
+            X_val, _, _, _ = self.normalize(X_val, mean, std, mean2, std2, feat_filt=feat_filt, norm=norm_method)
+            X_train, _, _, _ = self.normalize(X_train, mean, std, mean2, std2, feat_filt=feat_filt, norm=norm_method)
+            X_test, _, _, _ = self.normalize(X_test, mean, std, mean2, std2, feat_filt=feat_filt, norm=norm_method)
         else:
             X_tr, mean, std, feat_filt = self.normalize(X_tr, norm=norm_method)
-            filtered_feature_origin = feature_origin[feat_filt]
+            f_feature_origin = feature_origin[feat_filt]
+            f_feature_group = feature_group[feat_filt]
 
             X_val, _, _, _ = self.normalize(X_val, mean, std, feat_filt=feat_filt, norm=norm_method)
-            X_train, _, _, _ = self.normalize(X_train, mean, std, feat_filt=feat_filt, norm=norm_method)
+            X_train, _, _, _= self.normalize(X_train, mean, std, feat_filt=feat_filt, norm=norm_method)
             X_test, _, _, _ = self.normalize(X_test, mean, std, feat_filt=feat_filt, norm=norm_method)
 
         print("Dumping result..")
         with gzip.open('test%dval%dnorm%s.p.gz' % (self.test_fold, self.val_fold, norm_method), 'wb') as f:
-            pickle.dump((X_tr, X_val, X_train, X_test, y_tr, y_val, y_train, y_test, index_names, filtered_feature_origin), f)
+            pickle.dump((X_tr, X_val, X_train, X_test, y_tr, y_val, y_train, y_test, index_names, f_feature_origin, f_feature_group), f)
 
         return
 
 # Initialize
 preprocessor = Preprocessor(test_fold=0, val_fold=1)
 # Load and process data
-data = preprocessor.create_splits(norm_method='tanh_norm')
+data = preprocessor.create_splits(norm_method='tanh')
